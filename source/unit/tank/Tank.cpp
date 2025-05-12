bfa44cd8853f6418706d4c06c20f1a003dbd5216
Tóth András <andras.toth.0625@gmail.com>
2025-05-12 14:13:24 +0200

    tank unit megvalósítása

---
+++ source/unit/tank/Tank.cpp
@@ -0,0 +1,33 @@
+﻿#include "Tank.h"
+
+Tank::Tank() = default;
+
+Tank::Tank(int _hp, int _dmg) {
+    this->hp = _hp;
+    this->dmg = _dmg;
+    this->range = 1;
+}
+
+int Tank::getHp() {
+    return this->hp;
+}
+
+int Tank::getDmg() {
+    return this->dmg;
+}
+
+int Tank::getRange() {
+    return this->range;
+}
+
+void Tank::setHp(int _hp) {
+    this->hp = _hp;
+}
+
+void Tank::setDmg(int _dmg) {
+    this->dmg = _dmg;
+}
+
+void Tank::setRange(int _range) {
+    this->range = _range;
+}
---
+++ source/unit/tank/Tank.h
@@ -0,0 +1,26 @@
+﻿#ifndef TANK_H
+#define TANK_H
+
+
+
+class Tank {
+    int hp{};
+    int range{};
+    int dmg{};
+
+public:
+    Tank();
+    Tank(int _hp, int _dmg);
+
+    int getHp();
+    int getDmg();
+    int getRange();
+
+    void setHp(int _hp);
+    void setDmg(int _dmg);
+    void setRange(int _range);
+};
+
+
+
+#endif //TANK_H
