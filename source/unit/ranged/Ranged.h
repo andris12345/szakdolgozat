e78d12e67b4c7b45cc05d1b8708c09fdb1d88752
Tóth András <andras.toth.0625@gmail.com>
2025-05-12 14:11:22 +0200

    ranged unit megvalósítása

---
+++ source/unit/ranged/Ranged.h
@@ -0,0 +1,26 @@
+﻿#ifndef RANGED_H
+#define RANGED_H
+
+
+
+class Ranged {
+    int hp{};
+    int range{};
+    int dmg{};
+
+public:
+    Ranged();
+    Ranged(int _hp, int _dmg);
+
+    int getHp();
+    int getDmg();
+    int getRange();
+
+    void setHp(int _hp);
+    void setDmg(int _dmg);
+    void setRange(int _range);
+};
+
+
+
+#endif //RANGED_H
